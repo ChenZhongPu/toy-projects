c2a72ada85fcdc2d59d53ef5f398a79f36b84a14
CHEN Zhongpu <chenloveit@gmail.com>
2023-06-12 11:05:48 +0800

    update GB18030

--- c/gb-encoding/gb18030.c
+++ c/gb-encoding/gb18030.c
@@ -1,51 +1,10 @@
 #include "gb18030.h"
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
-bool check_by_range(char *data, size_t len) {
-  size_t i = 0;
-  while (i < len) {
-    uint8_t byte = (uint8_t)data[i];
-    if (byte <= 0x7F) {
-      // it is ASCII (single byte)
-      i++;
-    } else if (byte >= 0x81 && byte <= 0xFE) {
-      // double byte or four byte
-      if (i + 1 >= len) {
-        return false; // too short
-      }
-      uint8_t byte2 = (uint8_t)data[i + 1];
-      if (byte2 >= 0x40 && byte2 <= 0xFE && byte2 != 0x7F) {
-        // double byte
-        i += 2; // note that for double-byte, undefined has been mapped to PUA.
-      } else if (byte2 >= 0x30 && byte2 <= 0x39) {
-        // may be four byte
-        if (i + 3 >= len) {
-          return false; // too short
-        }
-        if ((byte >= 0x85 && byte <= 0x8F) || (byte >= 0xE4 && byte <= 0xFE)) {
-          return false; // reserved, user-defined
-        }
-        uint8_t byte3 = (uint8_t)data[i + 2];
-        uint8_t byte4 = (uint8_t)data[i + 3];
-        if (byte == 0x84 && ((byte2 << 16) + (byte3 << 8) + byte4) > 0x31A439) {
-          return false; // reserved
-        }
-        if (byte == 0xE3 && ((byte2 << 16) + (byte3 << 8) + byte4) > 0x329A35) {
-          return false;
-        }
-        i += 3;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  }
-  return true;
-}
-
-// eleven states
 typedef enum GB_State GB_State;
+// eleven states
 enum GB_State {
   VALID,
   MORE,       // it can be either double-byte or quad-byte
@@ -59,8 +18,7 @@ enum GB_State {
   BYTE_E3_32_9A,
   INVALID,
 };
-
-const uint8_t State_Transitions[11][256] = {
+static const GB_State State_Transitions[11][256] = {
     [VALID] =
         {
             [0x00 ... 0x7F] = VALID,
@@ -146,7 +104,140 @@ const uint8_t State_Transitions[11][256] = {
     [INVALID] = {[0x00 ... 0xFF] = INVALID},
 };
 
-inline GB_State update_state(uint8_t byte, GB_State *state) {
+bool check_by_range(char *data, size_t len) {
+  size_t i = 0;
+  while (i < len) {
+    uint8_t byte = (uint8_t)data[i];
+    if (byte <= 0x7F) {
+      // it is ASCII (single byte)
+      i++;
+    } else if (byte == 0x80 || byte == 0xFF) {
+      // invalid
+      return false;
+    } else {
+      // double-byte or quad-byte
+      if (i + 1 >= len) {
+        return false; // too short
+      }
+      uint8_t byte2 = (uint8_t)data[i + 1];
+      if (byte2 >= 0x40 && byte2 <= 0xFE && byte2 != 0x7F) {
+        // double-byte
+        i += 2;
+      } else if (byte2 < 0x30 || byte2 == 0x7F || byte2 == 0xFF) {
+        // invalid
+        return false;
+      } else {
+        // byte2 is from 0x30 to 0x39, it is a quad-byte
+        if (i + 3 >= len) {
+          return false; // too short
+        }
+        uint8_t byte3 = (uint8_t)data[i + 2];
+        if (byte3 < 0x81 || byte3 > 0xFE) {
+          return false;
+        }
+        uint8_t byte4 = (uint8_t)data[i + 3];
+        if (byte4 < 0x30 || byte4 > 0x39) {
+          return false;
+        }
+        /* if ((byte >= 0x81) && (byte <= 0x8F) && */
+        /*     (byte << 24) + (byte2 << 16) + (byte3 << 8) + byte4 > 0x8431A439)
+         * { */
+        /*   return false; // out of range */
+        /* } */
+        // is the following better?
+        if (byte > 0x84 && byte <= 0x8F) {
+          return false; // out of range
+        } else if (byte == 0x84) {
+          if (byte2 > 0x31)
+            return false;
+          else if (byte2 == 0x31) {
+            if (byte3 > 0xA4)
+              return false;
+            else if (byte3 == 0xA4) {
+              if (byte4 > 0x39)
+                return false;
+            }
+          }
+        }
+
+        /* if ((byte >= 0x90) && */
+        /*     (byte << 24) + (byte2 << 16) + (byte3 << 8) + byte4 > 0xE3329A35)
+         * { */
+        /*   return false; // out of range */
+        /* } */
+        if (byte > 0xE3)
+          return false;
+        else if (byte == 0xE3) {
+          if (byte2 > 0x32)
+            return false;
+          else if (byte2 == 0x32) {
+            if (byte3 > 0x9A)
+              return false;
+            else if (byte3 == 0x9A) {
+              if (byte4 > 0x35)
+                return false;
+            }
+          }
+        }
+        i += 3;
+      }
+    }
+  }
+  return true;
+}
+
+/* bool check_by_range(char *data, size_t len) { */
+/*   size_t i = 0; */
+/*   while (i < len) { */
+/*     uint8_t byte = (uint8_t)data[i]; */
+/*     if (byte <= 0x7F) { */
+/*       // it is ASCII (single byte) */
+/*       i++; */
+/*     } else if (byte >= 0x81 && byte <= 0xFE) { */
+/*       // double byte or four byte */
+/*       if (i + 1 >= len) { */
+/*         return false; // too short */
+/*       } */
+/*       uint8_t byte2 = (uint8_t)data[i + 1]; */
+/*       if (byte2 >= 0x40 && byte2 <= 0xFE && byte2 != 0x7F) { */
+/*         // double byte */
+/*         i += 2; // note that for double-byte, undefined has been mapped to
+ * PUA. */
+/*       } else if (byte2 >= 0x30 && byte2 <= 0x39) { */
+/*         // may be four byte */
+/*         if (i + 3 >= len) { */
+/*           return false; // too short */
+/*         } */
+/*         uint8_t byte3 = (uint8_t)data[i + 2]; */
+/*         if (byte3 < 0x81 || byte3 > 0xFE) { */
+/*           return false; */
+/*         } */
+/*         uint8_t byte4 = (uint8_t)data[i + 3]; */
+/*         if (byte4 < 0x30 || byte4 > 0x39) { */
+/*           return false; */
+/*         } */
+/*         if ((byte >= 0x81) && (byte <= 0x8F) && */
+/*             (byte << 24) + (byte2 << 16) + (byte3 << 8) + byte4 > 0x8431A439)
+ * { */
+/*           return false; // out of range */
+/*         } */
+/*         if ((byte >= 0x90) && */
+/*             (byte << 24) + (byte2 << 16) + (byte3 << 8) + byte4 > 0xE3329A35)
+ * { */
+/*           return false; // out of range */
+/*         } */
+/*         i += 3; */
+/*       } else { */
+/*         return false; */
+/*       } */
+/*     } else { */
+/*       return false; */
+/*     } */
+/*   } */
+/*   return true; */
+/* } */
+
+static inline GB_State update_state(uint8_t byte, GB_State *state) {
   *state = State_Transitions[*state][byte];
   return *state;
 }
